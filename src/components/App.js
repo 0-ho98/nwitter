89da42c31c204e0dfa9fbfbec9f767de09014b5b
(youngho) <one0374@naver.com>
2021-02-10 13:53:51 +0900

    Log In

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AppRouter from "components/Router";
 import {authService} from "myBase";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  //초기화가 됐는지 안됐는지 알려주는 state이다.
+  const [init, setInit] = useState(false);
+  // 로그인이 됐는지 안됐는지 알려주는 state이다.
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(()=>{
+    authService.onAuthStateChanged((user)=>{
+      if(user){
+        setIsLoggedIn(true);
+      }else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    })
+  },[])
   return (
     <>
-    <AppRouter isLoggedIn={isLoggedIn}/>
+    {init ? <AppRouter isLoggedIn={isLoggedIn}/>: "Initializing..."}
     <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
   );
